18a35deda941db51d298332521c9f8cbaad142de
jamilWebdesign2024 <jamiluddinjishan@gmail.com>
2025-06-14 09:46:27 +0600

    lll

--- src/Context/AuthContext/AuthProvider.jsx
+++ src/Context/AuthContext/AuthProvider.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { AuthContext } from '../AuthContext';
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
 import { auth } from '../../Firebase/Firebase.init';
+import axios from 'axios';
 
 const googleProvider = new GoogleAuthProvider();
 
@@ -29,6 +30,17 @@ const AuthProvider = ({children}) => {
         const unSubscribe = onAuthStateChanged(auth, currentUser=>{
             setUser(currentUser);
             setLoading(false);
+            if(currentUser?.email){
+                const userData = {email: currentUser.email};
+                axios.post('http://localhost:3000/jwt', userData, {
+                    withCredentials: true
+                })
+                .then(res=>{
+                    console.log(res.data);
+                })
+                .catch(error=>console.log(error)
+                )
+            }
             console.log('user in the auth state change', currentUser);
             
         })
